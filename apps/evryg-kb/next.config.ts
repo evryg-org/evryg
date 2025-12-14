15f229252d722c7d0c5902d4cd136cb77871ff2f
Jean-Baptiste Musso <jeanbaptiste@evryg.com>
2025-12-14 20:38:20 +0100

    Setup nextra plugin

--- apps/evryg-kb/next.config.ts
+++ apps/evryg-kb/next.config.ts
@@ -4,4 +4,12 @@ const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default nextConfig;
+import nextra from 'nextra'
+ 
+// Set up Nextra with its configuration
+const withNextra = nextra({
+  // ... Add Nextra-specific options here
+})
+ 
+// Export the final Next.js config with Nextra included
+export default withNextra(nextConfig)
