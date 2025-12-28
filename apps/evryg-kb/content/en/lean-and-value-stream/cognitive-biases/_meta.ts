ee969bd45750081ca77cfbc94231be5709319147
Jean-Baptiste Musso <jeanbaptiste@evryg.com>
2025-12-28 23:58:01 +0100

    Add article: Bus Factor (FR/EN)

--- apps/evryg-kb/content/en/lean-and-value-stream/cognitive-biases/_meta.ts
+++ apps/evryg-kb/content/en/lean-and-value-stream/cognitive-biases/_meta.ts
@@ -2,7 +2,8 @@ import type { MetaRecord } from 'nextra'
 
 const meta: MetaRecord = {
   index: 'Introduction',
-  'cargo-cult-when-imitation-is-not-enough': ''
+  'cargo-cult-when-imitation-is-not-enough': '',
+  'bus-factor-the-vulnerability-of-concentrated-knowledge': ''
 }
 
 export default meta
--- apps/evryg-kb/content/fr/lean-et-flux-de-valeur/biais-cognitifs/_meta.ts
+++ apps/evryg-kb/content/fr/lean-et-flux-de-valeur/biais-cognitifs/_meta.ts
@@ -2,7 +2,8 @@ import type { MetaRecord } from 'nextra'
 
 const meta: MetaRecord = {
   index: 'Introduction',
-  'cargo-cult-quand-imiter-ne-suffit-pas': ''
+  'cargo-cult-quand-imiter-ne-suffit-pas': '',
+  'bus-factor-la-vulnerabilite-du-savoir-concentre': ''
 }
 
 export default meta
