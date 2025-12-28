dd5a414d60852a2d7f9cd2d1ef1bb73c753129c0
Jean-Baptiste Musso <jeanbaptiste@evryg.com>
2025-12-28 23:11:36 +0100

    Add article: Continuous Delivery - a financial imperative (FR/EN)

--- apps/evryg-kb/content/en/lean-and-value-stream/continuous-delivery/_meta.ts
+++ apps/evryg-kb/content/en/lean-and-value-stream/continuous-delivery/_meta.ts
@@ -4,7 +4,8 @@ const meta: MetaRecord = {
   index: 'Introduction',
   'f-git-production-the-continuous-deployment-equation': '',
   'pull-requests-a-barrier-born-from-distrust': '',
-  'trunk-based-development-the-asymptotic-ideal': ''
+  'trunk-based-development-the-asymptotic-ideal': '',
+  'continuous-delivery-a-financial-imperative': ''
 }
 
 export default meta
--- apps/evryg-kb/content/fr/lean-et-flux-de-valeur/livraison-continue/_meta.ts
+++ apps/evryg-kb/content/fr/lean-et-flux-de-valeur/livraison-continue/_meta.ts
@@ -4,7 +4,8 @@ const meta: MetaRecord = {
   index: 'Introduction',
   'f-git-production-lequation-du-deploiement-continu': '',
   'pull-requests-une-barriere-nee-de-la-mefiance': '',
-  'trunk-based-development-lideal-asymptotique': ''
+  'trunk-based-development-lideal-asymptotique': '',
+  'continuous-delivery-un-enjeu-financier': ''
 }
 
 export default meta
