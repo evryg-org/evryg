39dc71d33e9e2058584799d84f1007dd134961a8
Jean-Baptiste Musso <jeanbaptiste@evryg.com>
2026-01-21 01:16:19 +0100

    Add GTM and Cookiebot tracking scripts to evryg-kb
    Adds a Scripts component that conditionally renders Cookiebot consent
    management and Google Tag Manager based on environment variables.
    Tracking is disabled by default and only enabled when
    NEXT_PUBLIC_ENABLE_TRACKING_SCRIPTS is set to "true".

---
+++ apps/evryg-kb/src/components/Scripts.tsx
@@ -0,0 +1,52 @@
+import Script from 'next/script'
+
+/**
+ * Tracking scripts component for Cookiebot and Google Tag Manager.
+ *
+ * Cookiebot loads first with manual blocking mode (required for Next.js).
+ * GTM is blocked until statistics consent is granted via Cookiebot.
+ *
+ * Environment variables:
+ * - NEXT_PUBLIC_ENABLE_TRACKING_SCRIPTS: Set to "true" to enable (production only)
+ * - NEXT_PUBLIC_COOKIEBOT_ID: Cookiebot domain group ID
+ * - NEXT_PUBLIC_GTM_ID: Google Tag Manager container ID
+ */
+export function Scripts() {
+  const enableTracking = process.env.NEXT_PUBLIC_ENABLE_TRACKING_SCRIPTS === 'true'
+  const cookiebotId = process.env.NEXT_PUBLIC_COOKIEBOT_ID
+  const gtmId = process.env.NEXT_PUBLIC_GTM_ID
+
+  if (!enableTracking || !cookiebotId || !gtmId) {
+    return null
+  }
+
+  return (
+    <>
+      {/* Cookiebot consent management - loads first */}
+      <Script
+        id="Cookiebot"
+        src="https://consent.cookiebot.com/uc.js"
+        data-cbid={cookiebotId}
+        data-blockingmode="manual"
+        strategy="afterInteractive"
+      />
+
+      {/* Google Tag Manager - blocked until statistics consent granted */}
+      <Script
+        id="gtm"
+        type="text/plain"
+        data-cookieconsent="statistics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','${gtmId}');
+          `,
+        }}
+      />
+    </>
+  )
+}
